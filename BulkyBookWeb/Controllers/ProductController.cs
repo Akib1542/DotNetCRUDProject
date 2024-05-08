329e4ba21840f74cfd21c077a148ac894a6882ef
Akib <ali.haider@brainstation-23.com>
2024-05-08 19:46:46 +0600

    Product Portion Add!

---
+++ BulkyBookWeb/Controllers/ProductController.cs
@@ -0,0 +1,141 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }   
+
+        public IActionResult Index()
+        {
+            IEnumerable<Products>productList=_db.Product;
+            return View(productList);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Products obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name!");
+
+            }
+            if (ModelState.IsValid)
+            {
+
+
+                _db.Product.Add(obj);
+                _db.SaveChanges();
+               
+                TempData["success"] = "Catagory Created Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var productFromDb = _db.Product.Find(id);
+            //var catagoryFromDbFirst = _db.Catagories.FirstOrDefault(c => c.Id == id);
+            //var catagoryFromDbSingle = _db.Catagories.SingleOrDefault(c => c.Id == id);
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+
+        }
+
+
+        //Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Products obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name!");
+
+            }
+            if (ModelState.IsValid)
+            {
+
+
+                _db.Product.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Catagory Edited Successfully!";
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //GET
+
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var productFromDb = _db.Product.Find(id);
+            //var catagoryFromDbFirst = _db.Catagories.FirstOrDefault(c => c.Id == id);
+            //var catagoryFromDbSingle = _db.Catagories.SingleOrDefault(c => c.Id == id);
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+
+        }
+
+
+
+        //Post Action Method
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Product.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Product.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Catagory Deleted Successfully!";
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
