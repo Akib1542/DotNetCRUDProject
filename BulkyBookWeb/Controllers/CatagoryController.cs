b0441678c1e9f86a3ed5b97d55136ea89b665dde
Akib <ali.haider@brainstation-23.com>
2024-05-13 15:53:39 +0600

    indentation added!

--- BulkyBookWeb/Controllers/CatagoryController.cs
+++ BulkyBookWeb/Controllers/CatagoryController.cs
@@ -9,8 +9,11 @@ namespace BulkyBookWeb.Controllers;
 
     public class CatagoryController : Controller
     {
-       
-        private readonly ICatagory catService;
+        #region Fields
+
+    private readonly ICatagory catService;
+
+    #endregion
 
         #region ctor
 
@@ -19,29 +22,33 @@ namespace BulkyBookWeb.Controllers;
             this.catService = catService;
 
         }
-    #endregion
+
+        #endregion
 
         #region Search
 
-    public async Task<IActionResult> Index(string? search)
+        public async Task<IActionResult> Index(string? search)
         {
        
             var data = await catService.GetCatBySearch(search);
             return View(data);
        
         }
-    #endregion
+
+        #endregion
 
 
         #region Create Get
 
-    public IActionResult Create()
+        public IActionResult Create()
         {                                              
             return View();
         }
-    #endregion
+
+        #endregion
 
         #region Create Post
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Catagory obj)
@@ -66,7 +73,8 @@ namespace BulkyBookWeb.Controllers;
             return View(obj);  
 
         }
-    #endregion
+
+        #endregion
 
 
         #region Edit GET
@@ -83,91 +91,96 @@ namespace BulkyBookWeb.Controllers;
             }
 
         }
-    #endregion
+
+        #endregion
 
 
         #region Edit POST
-    //Post Action Method
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(Catagory obj)
-    {
-        if (obj.Name == obj.DisplayOrder.ToString())
+        //Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Catagory obj)
         {
-            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name!");
-
-        }
-        if (ModelState.IsValid)
-        {
-
-            try
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
-                await catService.UpdateCatagoryAsync(obj);
-                TempData["success"] = "Catagory Edited Successfully!";
-                return RedirectToAction("Index");
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name!");
+
             }
-            catch
+            if (ModelState.IsValid)
             {
-                return View(obj);
-            }
+
+                try
+                {
+                    await catService.UpdateCatagoryAsync(obj);
+                    TempData["success"] = "Catagory Edited Successfully!";
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View(obj);
+                }
            
 
-            return RedirectToAction("Index");
-        }
-        return View(obj);
+                return RedirectToAction("Index");
+            }
+            return View(obj);
 
-    }
-    #endregion
+        }
+
+        #endregion
 
 
         #region Delete GET
-    public async Task<IActionResult> Delete(int id=0)
-    {
 
-        if (id == null || id == 0)
+        public async Task<IActionResult> Delete(int id=0)
         {
-            return NotFound();
-        }
 
-        try
-        {
-            var data = await catService.GetCatagoryAsync(id);
-            return View(data);
-        }
-        catch(Exception ex)
-        {
-            return NotFound();
-        }
-
-    }
-    #endregion
-
-        #region Delete POST
-
-    //Post Action Method
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeletePOST (int id=0)
-    {
-            if(id == 0)
+            if (id == null || id == 0)
             {
-               return NotFound();
+                return NotFound();
             }
+
             try
             {
-             
-                var data = await catService.DeleteCatagoryAsync(id);
-                TempData["success"] = "Catagory Deleted Successfully!";
-                return RedirectToAction("Index");
-       
+                var data = await catService.GetCatagoryAsync(id);
+                return View(data);
             }
             catch(Exception ex)
             {
-               return NotFound();
+                return NotFound();
             }
+
+        }
+
+        #endregion
+
+        #region Delete POST
+
+        //Post Action Method
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePOST (int id=0)
+        {
+                if(id == 0)
+                {
+                   return NotFound();
+                }
+                try
+                {
+             
+                    var data = await catService.DeleteCatagoryAsync(id);
+                    TempData["success"] = "Catagory Deleted Successfully!";
+                    return RedirectToAction("Index");
+       
+                }
+                catch(Exception ex)
+                {
+                   return NotFound();
+                }
             
-    }
-    #endregion
+        }
+
+        #endregion
 
 }
 
